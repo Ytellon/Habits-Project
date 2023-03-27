3ecee7ef3486ad852a14ae04e3633db480a2cdce
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-03-27 18:02:47 -0300

    feat: component home, header, includes svg r native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, 
   Inter_400Regular,
   Inter_600SemiBold,
@@ -27,7 +27,11 @@ export default function App() {
   return (
     <>
       <Home />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </>
   );
 }
