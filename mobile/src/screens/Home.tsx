eb717433783b280e07162c6d8c398ac161302321
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-03-28 18:06:56 -0300

    component habitDay

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,28 @@
- import { View } from 'react-native';
-import { HabitDay } from '../components/HabitDay';
+ import { View, Text } from 'react-native';
+import { HabitDay, DAY_SYZE } from '../components/HabitDay';
  import { Header } from '../components/Header';
  
+ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+
  export function Home() {
   return (
     <View className="flex bg-background px-8 pt-16">
       <Header />
+
+      <View className='flex-row mt-6 mb-2'>
+        {
+          weekDays.map((weekDay, i) => (
+            <Text
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold text-center"
+              style={{ width: DAY_SYZE }}
+            >
+              {weekDay}
+            </Text>
+          ))
+        }
+      </View>
       <HabitDay />
     </View>
   );
