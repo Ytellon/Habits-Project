6fb7e726b8b30b77b440c06f57768a8fc2339820
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-03-30 16:38:56 -0300

    feat: define routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -9,7 +9,7 @@ import { Habit } from '../screens/Habit';
 export function AppRoutes() {
   return (
 
-  <Navigator>
+  <Navigator screenOptions={{ headerShown: false }}>
     <Screen name="home" component={Home} />
     <Screen name="hew" component={New} />
     <Screen name="habit" component={Habit} />
